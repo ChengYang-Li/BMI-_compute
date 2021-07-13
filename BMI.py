46cb43aebd00eb63a5dbf42d807d7b87c23a1f8f
Li ChengYang <sinnerunoclay@gmail.com>
2021-07-14 01:09:32 +0800

    add comment

--- BMI.py
+++ BMI.py
@@ -1,7 +1,8 @@
+# 這是一個BMI計算器
 print('BMI計算')
 name = input('請輸入名字: ')
 print('你好啊', name)
 height = int(input('你的身高是多少公分?'))
 weight = int(input('你的體重是多少公斤?'))
-bmi = weight/((height/100)**2)
+bmi = weight/((height/100)**2) # BMI公式
 print('你的BMI為', bmi)
